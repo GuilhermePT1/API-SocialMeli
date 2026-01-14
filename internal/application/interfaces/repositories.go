0807bc5184dceb7db76aabf06162f75ef8a3bdb4
Guilherme <gui_parracho@hotmail.com>
2026-01-14 10:54:38 -0300

    Update interface para trabalhar com service e repositories está em conjunto com db

---
+++ internal/application/interfaces/repositories.go
@@ -0,0 +1,29 @@
+package interfaces
+
+import "github.com/GuilhermePT1/api-social-meli/internal/domain/models"
+
+// FOLLOW
+type FollowRepository interface {
+	Create(follow *models.Follow) error
+	Delete(userID, followerID uint) error
+	CountFollowers(userID uint) (int64, error)
+	FindFollowers(userID uint) ([]models.User, error)
+	FindFollowed(userID uint) ([]models.User, error)
+}
+
+// PRODUCT
+
+type ProductRepository interface {
+	Create(product *models.Product) error
+	FindById(id uint) (*models.Product, error)
+	FindAll() ([]models.Product, error)
+}
+
+// POST
+
+type PostRepository interface {
+	Create(post *models.Post) error
+	FindByUserLastTwoWeeks(userID uint) ([]models.Post, error)
+	FindPromoPosts() ([]models.Post, error)
+	CountPromoProducts() (int64, error)
+}
